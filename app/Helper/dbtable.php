cf4ca88da37176ca1793e9ff37e739a96a249769
Satendra Gurjar <78408436+satendragurjar844@users.noreply.github.com>
2024-02-27 11:31:33 +0530

    first commit

---
+++ app/Helper/dbtable.php
@@ -0,0 +1,14 @@
+<?php
+////////////////Define all table name here/////////////////////
+
+/*-------FOR CUSTOM DB SELECTION  PGSQL OR MYSQL ------*/
+define("_DB_", "pgsql");
+/*-----------------------------------------------------*/
+
+///////////////////////FOR AIR ONLY/////////////////////////////
+define("_AIR_RATE_MASTER_", "air.air_rate_master");
+define("_SEARCH_AIR_RATE_", "air.search_air_rate");
+
+///////////////////////END HERE/////////////////////////////?
+
+?>
